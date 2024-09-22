9732e76c908e1c113184be320103eec89a31e4f3
abhiram-ar <abhiram2001.ars@gmail.com>
2024-09-22 23:00:28 +0530

    local storage

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,18 +1,31 @@
 const search = document.getElementById('search');
 const title = document.getElementById('input-task-data');
 const date = document.getElementById('input-task-date');
+const datelabel = document.getElementById('date-label');
 const time = document.getElementById('input-task-time');
 const saveBtn = document.getElementById('save-to-do');
+const noList = document.querySelector('.no-list');
 
 const taskContainer = document.querySelector('.task-container');
-let isContainerEmpty = true;
-let laskList = JSON.parse(localStorage.getItem('taskList')) || [];
 
-// if(laskList.length === 0){
-//     taskContainer.innerHTML += `<div class="empty-todo">
-//             <img src="empty.png">
-//         </div>`;
-// }
+let laskList = JSON.parse(localStorage.getItem('taskList')) || [];
+taskContainer.innerHTML = "";
+
+if(laskList.length === 0){
+    noList.innerHTML += `<div class="empty-todo">
+            <img src="empty.png">
+        </div>`;
+}
+else{
+    laskList.forEach((task) => {
+        renderNewTask(task);
+    });
+}
+
+
+date.addEventListener('change', () => {
+    datelabel.innerText = date.value;
+})
 
 function getCurrentDate(){
     let today = new Date(Date.now());
@@ -29,21 +42,6 @@ function getFormattedDate(date){
     return `${month} ${targetDate.getDate()}`;
 }
 
-function taskTemplate(task){
-    return `<div class="task-item" id="${task.id}">
-                    <div>
-                        <input type="checkbox" ${task.checked ? "checked" : ""}><label for="${task.id}" class=${task.checked ? "checked" : ""}>${task.title}</label>
-                    </div>
-                    <div class="task-info">    
-                        <input type="time">
-                        <select value="">
-                            <option value="edit">Edit</option>
-                            <option value="delete">Delete</option>
-                        </select>
-                    </div>
-            </div>`
-}
-
 
 saveBtn.addEventListener('click', () => {
     let taskTitle = title.value;
@@ -53,6 +51,10 @@ saveBtn.addEventListener('click', () => {
     if(taskTitle===""){
         alert('Please enter task data');
         return
+    }
+
+    if(taskTime === ""){
+        taskTime = "00:00";
     }
 
     if(taskDate === ""){
@@ -70,71 +72,199 @@ saveBtn.addEventListener('click', () => {
 
     laskList.push(task);
     console.log(laskList);
-    renderTask(task);
+    renderNewTask(task);
 
 
-    //localStorage.setItem('taskList', JSON.stringify(laskList));
-
-    taskTitle = "";
-    taskDate = "";
-    taskTime = "";
-
+    
+    title.value = "";
+    date.value = "";
+    time.value = "";
+    
+    localStorage.setItem('taskList', JSON.stringify(laskList));
 });
 
 
-function renderTask(task){
-    console.log(taskContainer.children);
-    if(isContainerEmpty){
-        taskContainer.innerHTML = "";
-        isContainerEmpty = false;
-
-        let newTaskGroup = document.createElement('div');
-        newTaskGroup.className = 'task-group';
-        newTaskGroup.id = task.date;
-        
-        let groupName = document.createElement('h2');
-        groupName.id = task.date;
-        if(task.date === getCurrentDate()){
-            groupName.innerHTML = "Today";
-        }
-        else{
-            groupName.innerHTML = getFormattedDate(task.date);
-        }
-        newTaskGroup.appendChild(groupName);
-
-        // render new task details
-
-        newTaskGroup.innerHTML += taskTemplate(task);
-
-        taskContainer.appendChild(newTaskGroup);
+function renderNewTask(task){
+    if(taskContainer.children.length === 0){
+        noList.innerHTML = "";
+        createNewTaskGroupAndAddTask(task);
     }
     else{
         let taskGroup = document.getElementById(task.date);
         if(taskGroup === null){
-            let newTaskGroup = document.createElement('div');
-            newTaskGroup.className = 'task-group';
-            newTaskGroup.id = task.date;
-            
-            let groupName = document.createElement('h2');
-            groupName.id = task.date;
-            if(task.date === getCurrentDate()){
-                groupName.innerHTML = "Today";
-            }
-            else{
-                groupName.innerHTML = getFormattedDate(task.date);
-            }
-            newTaskGroup.appendChild(groupName);
-
-            // render new task details
-
-            newTaskGroup.innerHTML += taskTemplate(task);
-
-            taskContainer.appendChild(newTaskGroup);
+            createNewTaskGroupAndAddTask(task);
         }
         else{
-            taskGroup.innerHTML += taskTemplate(task);
+
+            let newTask = document.createElement('div');
+            newTask.className = 'task-item';
+            newTask.id = task.id;
+
+            let checkboxDiv = document.createElement('div');
+            let checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.id = task.id + "c";
+            checkbox.checked = task.checked;
+            checkboxDiv.append(checkbox);
+            let label = document.createElement('label');
+            label.htmlFor = task.id + "c";
+            label.innerText = task.title;
+            label.className = task.checked ? "checked" : "";
+            checkboxDiv.appendChild(label);
+
+            newTask.appendChild(checkboxDiv);
+
+            let taskInfo = document.createElement('div');
+            taskInfo.className = 'task-info';
+            let timeInput = document.createElement('input');
+            timeInput.type = 'time';
+            timeInput.value = task.time;
+            let select = document.createElement('select');
+            let editOption = document.createElement('option');
+            editOption.value = 'edit';
+            editOption.innerText = 'Edit';
+            let deleteOption = document.createElement('option');
+            deleteOption.value = 'delete';
+            deleteOption.innerText = 'Delete';
+            
+            select.appendChild(editOption);
+            select.appendChild(deleteOption);
+            taskInfo.appendChild(timeInput);
+            taskInfo.appendChild(select);
+            newTask.appendChild(taskInfo);
+            taskGroup.appendChild(newTask);
+
+            checkbox.addEventListener('change', (e) => {
+                console.log(e.target.checked);
+                if(e.target.checked){
+                    label.className = "checked";
+                    let index = laskList.findIndex((item) => item.id === task.id);
+                    laskList[index].checked = true
+                    localStorage.setItem('taskList', JSON.stringify(laskList));
+                }
+                else{
+                    label.className = "";
+                    let index = laskList.findIndex((item) => item.id === task.id);
+                    laskList[index].checked = false 
+                    localStorage.setItem('taskList', JSON.stringify(laskList));
+
+                }
+            }); 
+
+            //delete and edit task
+            select.addEventListener('change', (e) => {
+                if(e.target.value === 'edit'){
+                    console.log('edit');
+                }
+                else if(e.target.value === 'delete'){
+                    console.log('delete');
+                    let taskGroup = document.getElementById(task.date);
+                    taskGroup.removeChild(newTask);
+                    if(taskGroup.children.length === 1){
+                        taskContainer.removeChild(taskGroup);
+                    }
+                    let index = laskList.findIndex((item) => item.id === task.id);
+                    laskList.splice(index, 1);
+                    if(laskList.length === 0){
+                    noList.innerHTML += `<div class="empty-todo">
+                        <img src="empty.png">
+                    </div>`;
+            }
+                    localStorage.setItem('taskList', JSON.stringify(laskList));
+                }
+            });
         }
     }
+
+}
+
+function createNewTaskGroupAndAddTask(task){
+    let newTaskGroup = document.createElement('div');
+    newTaskGroup.className = 'task-group';
+    newTaskGroup.id = task.date;
+        
+    let groupName = document.createElement('h2');
+    if(task.date === getCurrentDate()) groupName.innerHTML = "Today";
+    else groupName.innerHTML = getFormattedDate(task.date);
+    newTaskGroup.appendChild(groupName);
+
+    // render new task details
+    let newTask = document.createElement('div');
+    newTask.className = 'task-item';
+    newTask.id = task.id;
+
+    let checkboxDiv = document.createElement('div');
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = task.id + "c";
+    checkbox.checked = task.checked;
+    checkboxDiv.append(checkbox);
+    let label = document.createElement('label');
+    label.htmlFor = task.id + "c";
+    label.innerText = task.title;
+    label.className = task.checked ? "checked" : "";
+    checkboxDiv.appendChild(label);
+    newTask.appendChild(checkboxDiv);
+
+    let taskInfo = document.createElement('div');
+    taskInfo.className = 'task-info';
+    let timeInput = document.createElement('input');
+    timeInput.type = 'time';
+    timeInput.value = task.time;
+    let select = document.createElement('select');
+    let editOption = document.createElement('option');
+    editOption.value = 'edit';
+    editOption.innerText = 'Edit';
+    let deleteOption = document.createElement('option');
+    deleteOption.value = 'delete';
+    deleteOption.innerText = 'Delete';
+    
+    select.appendChild(editOption);
+    select.appendChild(deleteOption);
+    taskInfo.appendChild(timeInput);
+    taskInfo.appendChild(select);
+    newTask.appendChild(taskInfo);
+    newTaskGroup.appendChild(newTask);
+    taskContainer.appendChild(newTaskGroup);
+
+    checkbox.addEventListener('change', (e) => {
+        console.log(e.target.checked);
+        if(e.target.checked){
+            label.className = "checked";
+            let index = laskList.findIndex((item) => item.id === task.id);  
+            laskList[index].checked = true
+            localStorage.setItem('taskList', JSON.stringify(laskList));
+        }
+        else{
+            label.className = "";
+            let index = laskList.findIndex((item) => item.id === task.id);
+            laskList[index].checked = false
+            localStorage.setItem('taskList', JSON.stringify(laskList));
+        }
+    }); 
+
+    //delete and edit task
+    select.addEventListener('change', (e) => {
+        if(e.target.value === 'edit'){
+            console.log('edit');
+        }
+        else if(e.target.value === 'delete'){
+            console.log('delete');
+            let taskGroup = document.getElementById(task.date);
+            taskGroup.removeChild(newTask);
+            if(taskGroup.children.length === 1){
+                taskContainer.removeChild(taskGroup);
+            }
+            let index = laskList.findIndex((item) => item.id === task.id);
+            laskList.splice(index, 1);
+            if(laskList.length === 0){
+                noList.innerHTML += `<div class="empty-todo">
+                        <img src="empty.png">
+                    </div>`;
+            }
+            localStorage.setItem('taskList', JSON.stringify(laskList));
+        }
+    });
 
 }
 
